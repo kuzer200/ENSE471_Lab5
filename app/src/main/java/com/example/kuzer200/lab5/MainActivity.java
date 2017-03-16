9174736341b9977971ffc38bc7b3289656405747
kuzer200 <kuzer200@uregina.ca>
2017-03-16 11:22:39 -0600

    Initial commit

---
+++ app/src/main/java/com/example/kuzer200/lab5/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.kuzer200.lab5;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /** Called when the user taps the Send button */
+    public void logIn(View view) {
+        Intent intent = new Intent(this, LoginConfirmActivity.class);
+        startActivity(intent);
+
+    }
+
+}
